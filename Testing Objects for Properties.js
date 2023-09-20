41ea22a785441bda58929117b18549e81749d510
cool-rex <rbrefo2@gmail.com>
2023-09-20 23:29:26 +0000

    update

--- Testing Objects for Properties.js
+++ Testing Objects for Properties.js
@@ -1,1 +1,16 @@
-// Modify the function checkObj to test if the object passed to the function parameter obj contains the specific property passed to the function parameter checkProp. If the property passed to checkProp is found on obj, return that property's value. If not, return Not Found.
+// Modify the function checkObj to test if the object passed to the function parameter obj contains the specific property passed to the function parameter checkProp. If the property passed to checkProp is found on obj, return that property's value. If not, return Not Found.function checkObj(obj, checkProp) {
+ 
+
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if (obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp];
+    } else {
+      return "Not Found";
+    }
+  
+  
+    // return "Change Me!";
+    // Only change code above this line
+  }
